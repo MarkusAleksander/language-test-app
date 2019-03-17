2f40a5532ce2beffefd99fc1d134194abd5efd19
Markus <markholden1989@googlemail.com>
2019-03-17 16:28:14 +0000

    Minor styling adjustment

--- src/components/ModulesSelector/ModuleItem.vue
+++ src/components/ModulesSelector/ModuleItem.vue
@@ -16,19 +16,43 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .module-item {
+
+    $wrapper: &;
+
     padding: 1.5rem;
+
+    .module-item-image {
+        width: 7.5rem;
+        height: 7.5rem;
+        border-radius: 50%;
+        border: 1px dashed rgb(70,70,70);
+        margin: 0 auto 1rem;
+
+        &:hover,
+        &:focus {
+            cursor: pointer;
+            animation-name: rotate;
+            animation-duration: 8s;
+            animation-timing-function: linear;
+        }
+    }
+    .module-item-title {
+        text-align: center;
+        font-size: 1.5rem;
+    }
 }
-.module-item-image {
-    width: 7.5rem;
-    height: 7.5rem;
-    border-radius: 50%;
-    border: 1px dashed rgb(70,70,70);
-    margin: 0 auto 1rem;
-}
-.module-item-title {
-    text-align: center;
-    font-size: 1.5rem;
+
+@keyframes rotate {
+    0% {
+        transform: rotate(0deg);
+    }
+    50% {
+        transform: rotate(180deg);
+    }
+    100% {
+        transform: rotate(359deg);
+    }
 }
 </style>
